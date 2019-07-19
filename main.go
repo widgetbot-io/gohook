9af529511501a3f3a0797688fc634a8359dfc0b2
Daave <daave@aaathats3as.com>
2019-07-19 21:32:35 +0100

    Yoink

--- main.go
+++ main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"git.deploys.io/disweb/gohook/utils"
-	"github.com/gorilla/mux"
-	"net/http"
-)
-
 func main() {
-	embed := utils.NewEmbed()
-	embed.SetTitle("Title")
-	embed.SetDescription("Description")
 
-	router := mux.NewRouter()
-
-	setupRoutes(router)
-	_ = http.ListenAndServe(":8443", nil)
-}
-
-func setupRoutes(router *mux.Router) {
-	router.HandleFunc("/api/hook/{ID}/{Secret}/{Provider}/{Options}", func(w http.ResponseWriter, r *http.Request) {
-		params := mux.Vars(r)
-		fmt.Print(params)
-
-	})
-
-	http.Handle("/", router)
 }
