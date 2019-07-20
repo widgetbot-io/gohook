b81cfe09d515257d7b98015223ebe1c8d56888fd
Daave <daave@aaathats3as.com>
2019-07-20 02:24:13 +0100

    GitLab push event nearly finished. All util functions

--- providers/gitlab/push.go
+++ providers/gitlab/push.go
@@ -1,9 +1,60 @@
 package gitlab
 
 import (
+	"fmt"
 	"git.deploys.io/disweb/gohook/structs"
+	"git.deploys.io/disweb/gohook/utils"
+	"github.com/sirupsen/logrus"
+	webhook "gopkg.in/go-playground/webhooks.v5/gitlab"
+	"strings"
 )
 
-func PushHandler(r structs.Context) {
+func PushHandler(c structs.EventContext) error {
+	payload := c.Payload.(webhook.PushEventPayload)
+	branch := utils.GetBranch(payload.Ref)
 
+	commit := ""
+	if len(payload.Commits) == 1 {
+		commit = "1 commit"
+	} else {
+		commit = fmt.Sprintf("%d commits", len(payload.Commits))
+	}
+
+	embed := utils.NewEmbed().
+		SetTitle(fmt.Sprintf("[%s:%s] %s", payload.Project.Name, branch, commit)).
+		SetColour(0x0089ee).
+		// SetURL(payload.Repository.URL).
+		SetAuthor(payload.UserName, payload.UserAvatar)
+
+	// var reduced []webhook.Commit
+	var authors []string
+
+	for _, commit := range payload.Commits {
+		if utils.IndexOfAuthor(commit.Author.Name, authors) == -1 {
+			authors = append(authors, commit.Author.Name)
+		}
+		for _, author := range authors {
+			if commit.Author.Name == author {
+
+			}
+		}
+	}
+
+	logrus.Info(authors)
+
+	for _, commit := range payload.Commits {
+		commitString := ""
+		if strings.HasPrefix(commit.Message, "!") || strings.HasPrefix(commit.Message, "$") {
+			commitString = "This commit message has been marked as private."
+		} else {
+			commitString = commit.Message
+		}
+
+		embed.AddField(fmt.Sprintf("Commit from %s", commit.Author.Name), commitString, false)
+	}
+
+	logrus.Info(len(payload.Commits))
+	logrus.Info(len(embed.Fields))
+
+	return utils.SendToDiscord(c.ID, c.Secret, embed)
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,10 @@
 package utils
 
 import (
+	"bytes"
+	"encoding/json"
 	"git.deploys.io/disweb/gohook/structs"
+	"net/http"
 	"strings"
 )
 
@@ -21,4 +24,39 @@ func IndexOfEvent(element string, data []structs.Event) int {
 		}
 	}
 	return -1
+}
+
+func IndexOfAuthor(element string, data []string) int {
+	for k, v := range data {
+		if strings.ToLower(element) == strings.ToLower(v) {
+			return k
+		}
+	}
+	return -1
+}
+
+func GetBranch(ref string) string {
+	return strings.Join(strings.Split(ref, "/")[2:], "/")
+}
+
+func GroupBy(arrayToGroups []string, key string, key2 string) []string {
+	return []string{""}
+}
+
+type Embeds struct {
+	Embeds []*Embed `json:"embeds"`
+}
+
+func SendToDiscord(ID string, secret string, embed *Embed) error {
+	embeds := Embeds{}
+	embeds.Embeds = []*Embed{embed}
+
+	jsonBytes, _ := json.Marshal(embeds)
+
+	_, err := http.Post("https://canary.discordapp.com/api/webhooks/586650758607536165/r21LYTWAlVYTQ_EHMBjPlK5f8S66p-IFFMXmv-lOqH-gMD-jD4n2kYO5dAhFtKUGiv98", "application/json", bytes.NewBuffer(jsonBytes))
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
