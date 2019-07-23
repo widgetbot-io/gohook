e845e1fd331eb2f5c04116cd60e706d724723031
Daave <daave@aaathats3as.com>
2019-07-23 22:07:27 +0100

    GitHub push

--- providers/github/push.go
+++ providers/github/push.go
@@ -1,21 +1,46 @@
 package github
 
 import (
+	"fmt"
 	"git.deploys.io/disweb/gohook/structs"
 	"git.deploys.io/disweb/gohook/utils"
 	webhook "gopkg.in/go-playground/webhooks.v5/github"
+	"strings"
 )
 
 func PushHandler(c structs.EventContext) error {
 	payload := c.Payload.(webhook.PushPayload)
+	branch := utils.GetBranch(payload.Ref)
+
+	if strings.HasPrefix(branch, "!") || strings.HasPrefix(branch, "$") {
+		return nil
+	}
+
+	commit := ""
+	if len(payload.Commits) == 1 {
+		commit = "1 commit"
+	} else {
+		commit = fmt.Sprintf("%d commits", len(payload.Commits))
+	}
 
 	embed := utils.NewEmbed().
-		SetTitle("Ping event recieved!").
-		SetAuthor(payload.Sender.Login, payload.Sender.AvatarURL).
-		SetDescription("Webhook successfully linked to repository.").
+		SetTitle(fmt.Sprintf("[%s:%s] %s", payload.Repository.Name, branch, commit)).
+		SetColour(0x0089ee).
+		SetFooter("Gohook", "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png").
+		SetTimestamp().
 		SetURL(payload.Repository.HTMLURL).
-		SetColour(utils.RandomColor()).
-		SetTimestamp()
+		SetAuthor(payload.Pusher.Name, payload.Sender.AvatarURL)
+
+	for _, commit := range payload.Commits {
+		commitString := ""
+		if strings.HasPrefix(commit.Message, "!") || strings.HasPrefix(commit.Message, "$") {
+			commitString = "This commit message has been marked as private."
+		} else {
+			commitString = commit.Message
+		}
+
+		embed.AddField(fmt.Sprintf("Commit from %s", commit.Author.Name), fmt.Sprintf("[`%s`](%s) %s", commit.ID[:7], commit.URL, commitString), false)
+	}
 
 	return utils.SendToDiscord(c.ID, c.Secret, embed)
 }
