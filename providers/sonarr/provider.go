8e31875521276a7dcf2e150681cb8cce06f8d6c3
Daave <daave@aaathats3as.com>
2019-08-10 00:58:35 +0100

    Remove random HTTP check

--- providers/sonarr/provider.go
+++ providers/sonarr/provider.go
@@ -3,16 +3,11 @@ package sonarr
 import (
 	"encoding/json"
 	"git.deploys.io/disweb/gohook/structs"
-	"git.deploys.io/disweb/gohook/utils"
-	"net/http"
 )
 
 func Handler(c structs.ProviderContext) error {
 	var TestPayload structs.SonarrTest
 
-	if c.Context.Request.Method != http.MethodPost {
-		return utils.ErrInvalidHTTPMethod
-	}
 	_ = json.Unmarshal([]byte(c.Payload), &TestPayload)
 
 	return c.Event.Handler(structs.EventContext{
